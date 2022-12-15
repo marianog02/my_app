198d7fe47b66ee78ac35f5af5c1badd3fb0a6e1e
Mariano <mariano.geyne@gmail.com>
2022-12-14 22:54:08 -0600

    ,,

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,12 @@ const jwt = require("jsonwebtoken")
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
-        console.log(req.headers.authorization)
+        
         const decoded = jwt.verify(token, "debugkey")
         req.user = decoded
         next()
     } catch (error) {
+        console.log(req.headers.authorization)
         res.status(401).json({ code: 401, message: "No tienes permiso" + error })
     }
 }
